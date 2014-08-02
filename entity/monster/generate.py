863299375a6dca5c657d882af086c306e52e12c2
DND91 <Jonathan.Crusoe@Gmail.com>
2014-08-02 15:24:49 +0200

    Base

---
+++ entity/monster/generate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+'''
+
+    Monster generation
+    Dung Points(DP) = For buying a monster like we have Treasure points for Items.
+    Monster = Entity + AI + Race + Profession + Inventory With Items
+    Monster Generation want to work with Race, Profession and Inventory and give an abstract thought on what
+    the AI should be (Brave, Coward, Berserker and so on).
+
+'''
+
