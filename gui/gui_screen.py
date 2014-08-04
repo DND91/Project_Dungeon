c973e62a6be6cec2b437c085d3ff7b4e748c7d9d
DND91 <Jonathan.Crusoe@Gmail.com>
2014-08-04 16:11:41 +0200

    Base

--- gui/gui_screen.py
+++ gui/gui_screen.py
@@ -6,10 +6,11 @@ class GuiScreen(gobj.GuiObject):
     def __init__(self, x, y, width, height):
         super().__init__(x,y,width,height)
     
-    
-    
     def update(self, game, delta):
         x = 0
     
     def draw(self, ps, game):
         x = 0
+    
+    def mouseClick(self, game, screen, rect):
+        return False
